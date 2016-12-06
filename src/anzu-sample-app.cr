a63ba4b9a8ca0b1d2bb79323ddaee4e8460b5c18
Hexa <hexa.diary@gmail.com>
2016-12-06 22:03:34 +0900

    コスメ

--- src/anzu-sample-app.cr
+++ src/anzu-sample-app.cr
@@ -9,11 +9,11 @@ ANZU_API_KEY = ""
 ANZU_SECRET_KEY = ""
 ANZU_API_URI = "https://anzu.shiguredo.jp/api/"
 
-def generate_request_headers(target, secret_key, api_key)
+def generate_request_headers(target)
   date = Time.utc_now.to_s("%Y-%m-%dT%H:%M:%SZ")
-  signature = generate_signature(date, secret_key, api_key)
+  signature = generate_signature(date, ANZU_SECRET_KEY, ANZU_API_KEY)
   HTTP::Headers{"x-anzu-target" => target,
-                "x-anzu-apikey" => api_key,
+                "x-anzu-apikey" => ANZU_API_KEY,
                 "x-anzu-date" => date,
                 "x-anzu-signature" => signature,
                 "Content-Type" => "application/json"}
@@ -32,7 +32,7 @@ get "/upstream_token/" do
 end
 
 get "/downstream_token/" do |env|
-  headers = generate_request_headers("AnzuAPI_20151216.GenerateDownstreamToken", ANZU_SECRET_KEY, ANZU_API_KEY)
+  headers = generate_request_headers("AnzuAPI_20151216.GenerateDownstreamToken")
   body = {channelId: CHANNEL_ID}.to_json
   response = HTTP::Client.post(ANZU_API_URI, headers: headers, body: body)
   env.response.status_code = response.status_code
@@ -41,7 +41,7 @@ get "/downstream_token/" do |env|
 end
 
 post "/disconnect/" do |env|
-  headers = generate_request_headers("AnzuAPI_20151216.Disconnect", ANZU_SECRET_KEY, ANZU_API_KEY)
+  headers = generate_request_headers("AnzuAPI_20151216.Disconnect")
   client_id = env.params.json["clientId"]
   body = {channelId: CHANNEL_ID, clientId: client_id}.to_json
   response = HTTP::Client.post(ANZU_API_URI, headers: headers, body: body)
@@ -51,7 +51,7 @@ post "/disconnect/" do |env|
 end
 
 post "/disconnect_channel/" do |env|
-  headers = generate_request_headers("AnzuAPI_20151216.DisconnectChannel", ANZU_SECRET_KEY, ANZU_API_KEY)
+  headers = generate_request_headers("AnzuAPI_20151216.DisconnectChannel")
   body = {channelId: CHANNEL_ID}.to_json
   response = HTTP::Client.post(ANZU_API_URI, headers: headers, body: body)
   env.response.status_code = response.status_code
@@ -60,7 +60,7 @@ post "/disconnect_channel/" do |env|
 end
 
 get "/list_connections/" do |env|
-  headers = generate_request_headers("AnzuAPI_20151216.ListConnections", ANZU_SECRET_KEY, ANZU_API_KEY)
+  headers = generate_request_headers("AnzuAPI_20151216.ListConnections")
   body = {channelId: CHANNEL_ID}.to_json
   response = HTTP::Client.post(ANZU_API_URI, headers: headers, body: body)
   env.response.status_code = response.status_code
@@ -69,7 +69,7 @@ get "/list_connections/" do |env|
 end
 
 get "/list_channels/" do |env|
-  headers = generate_request_headers("AnzuAPI_20151216.ListChannels", ANZU_SECRET_KEY, ANZU_API_KEY)
+  headers = generate_request_headers("AnzuAPI_20151216.ListChannels")
   response = HTTP::Client.post(ANZU_API_URI, headers: headers)
   env.response.status_code = response.status_code
   env.response.content_type = "application/json"
@@ -77,7 +77,7 @@ get "/list_channels/" do |env|
 end
 
 post "/regenerate_upstream_token/" do |env|
-  headers = generate_request_headers("AnzuAPI_20151216.RegenerateUpstreamToken", ANZU_SECRET_KEY, ANZU_API_KEY)
+  headers = generate_request_headers("AnzuAPI_20151216.RegenerateUpstreamToken")
   body = {channelId: CHANNEL_ID}.to_json
   response = HTTP::Client.post(ANZU_API_URI, headers: headers, body: body)
   env.response.status_code = response.status_code
